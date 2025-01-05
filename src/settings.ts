4307510ae8074f305d6c87b74c214ec9169ffc48
Théo Gil <theo@epic.net>
2025-01-05 14:23:06 +0100

    remove gaps

--- src/settings.ts
+++ src/settings.ts
@@ -36,7 +36,7 @@ const DEBUG_PARAMS = {
     friction: 0,
     frictionAir: 0,
     frictionStatic: 0,
-    gaps: { min: 400, max: 500 },
+    gaps: { min: 0, max: 0 },
     ties: {
       width: 10,
       height: 5,
