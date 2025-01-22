195bf15bd39368752d2aa3f2744dd19cad2184cf
Theo Gil <theo@epic.net>
2025-01-22 15:20:12 +0100

    start screen entrance exit aniamtions

--- src/modules/UI.ts
+++ src/modules/UI.ts
@@ -1,3 +1,4 @@
+import gsap from "gsap";
 import { formatTime } from "../utils/time";
 import { emitter } from "./emitter";
 
@@ -7,11 +8,95 @@ const PILL_EL_ACTIVE_CLASS = "active";
 const UI = {
   startScreen: {
     el: document.querySelector(".js-start-screen") as HTMLElement,
+    entranceAnimation: null as gsap.core.Timeline | null,
+    exitAnimation: null as gsap.core.Timeline | null,
     animateIn: () => {
       UI.startScreen.el.style.display = "flex";
+      UI.startScreen.entranceAnimation?.resume(0);
     },
     animateOut: () => {
-      UI.startScreen.el.style.display = "none";
+      UI.startScreen.entranceAnimation?.kill();
+
+      UI.startScreen.exitAnimation?.resume(0).then(() => {
+        UI.startScreen.el.style.display = "none";
+      });
+    },
+    initEntranceAnimation: () => {
+      const TL = gsap.timeline({
+        paused: true,
+      });
+
+      TL.fromTo(
+        UI.startScreen.el,
+        {
+          clipPath: "inset(50% 50% 50% 50%)",
+        },
+        {
+          clipPath: "inset(0% 0% 0% 0%)",
+          ease: "power4.inOut",
+          clearProps: true,
+        }
+      );
+
+      const animateInTargets =
+        UI.startScreen.el.querySelectorAll("[data-animate-in]");
+
+      TL.fromTo(
+        animateInTargets,
+        {
+          opacity: 0,
+          y: 40,
+        },
+        {
+          opacity: 1,
+          y: 0,
+          stagger: 0.1,
+          duration: 1.25,
+          ease: "elastic.out(1,0.3)",
+          clearProps: true,
+        },
+        "-=0.25"
+      );
+
+      return TL;
+    },
+    initExitAnimation: () => {
+      const TL = gsap.timeline({
+        paused: true,
+      });
+
+      const animateInTargets = [
+        ...UI.startScreen.el.querySelectorAll("[data-animate-in]"),
+      ].reverse();
+
+      TL.fromTo(
+        animateInTargets,
+        {
+          opacity: 1,
+          y: 0,
+        },
+        {
+          opacity: 0,
+          y: 40,
+          stagger: 0.05,
+          duration: 0.5,
+          ease: "power4.out",
+        }
+      );
+
+      TL.fromTo(
+        UI.startScreen.el,
+        {
+          clipPath: "inset(0% 0% 0% 0%)",
+        },
+        {
+          clipPath: "inset(50% 50% 50% 50%)",
+          ease: "power4.out",
+        },
+        0.15
+      );
+
+      return TL;
     },
   },
   endScreen: {
@@ -69,6 +154,11 @@ const UI = {
     restartBtnEl.addEventListener("click", () => {
       emitter.emit("ui_restartGame");
     });
+
+    this.startScreen.exitAnimation = this.startScreen.initExitAnimation();
+
+    this.startScreen.entranceAnimation =
+      this.startScreen.initEntranceAnimation();
 
     this.startScreen.animateIn();
   },
