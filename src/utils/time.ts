694fcb02403e9ac9fe2afb4a14061d768cea7c7f
Theo Gil <theo@epic.net>
2025-01-23 10:37:55 +0100

    refactor: update time format

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,12 +3,10 @@
  */
 const formatTime = (time: number) => {
   const s = Math.floor(time / 1000);
-  const ms = (time / 1000) % 1;
 
   const formattedSeconds = s.toString().padStart(2, "0");
-  const formattedMilliseconds = ms.toFixed(2).substring(2);
 
-  return `${formattedSeconds}"${formattedMilliseconds}`;
+  return `${formattedSeconds}”`;
 };
 
 export { formatTime };
